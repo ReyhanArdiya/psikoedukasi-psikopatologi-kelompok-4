053bca1ab6b6f53e191b273bbdd719182039dd1f
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-11 15:08:10 +0700

    Create `AcrylicDouble`

---
+++ src/components/UI/Acrylics/AcrylicDouble.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import AcrylicSingle from "./AcrylicSingle";
+
+const Container = styled.div`
+    position: relative;
+    font-size: 1em;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    width: max-content;
+
+    .acrylic-single:first-of-type {
+        z-index: 2;
+    }
+`;
+
+const SmallerAcrylic = styled(AcrylicSingle)`
+    position: absolute;
+    bottom: -20%;
+    min-height: 67.56%;
+    min-width: 82%;
+    z-index: 1;
+`;
+
+const AcrylicDouble = ({ children, className = "" }) => {
+	return (
+		<Container className={`acrylic-double ${className}`}>
+			<AcrylicSingle>{children}</AcrylicSingle>
+			<SmallerAcrylic />
+		</Container>
+	);
+};
+
+export default AcrylicDouble;
