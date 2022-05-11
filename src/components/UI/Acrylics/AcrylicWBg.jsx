b9babd327ef3b44222e028b3647900eba1cb95cc
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-11 21:51:15 +0700

    Create `AcrylicWBg`

---
+++ src/components/UI/Acrylics/AcrylicWBg.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import AcrylicSingle from "./AcrylicSingle";
+import blackAcrylicPaint from "../../../images/content/black-acrylic-paint.jpg";
+
+const Container = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    position: relative;
+    background: fixed black url(${blackAcrylicPaint}) center/100% 100% no-repeat;
+    width: max-content;
+    height: max-content;
+`;
+
+
+const AcrylicWBg = ({ className = "" }) => {
+	return (
+		<Container className={`acrylic-bg ${className}`}>
+			<AcrylicSingle />
+		</Container>
+	);
+};
+
+export default AcrylicWBg;
