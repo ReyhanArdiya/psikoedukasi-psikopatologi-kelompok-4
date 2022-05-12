5e4fe802cdeeb975d6bf104db0252bc375c7cb4b
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-12 16:01:53 +0700

    Finish `ProfileCard`

--- src/components/UI/ProfileCard/ProfileCard.jsx
+++ src/components/UI/ProfileCard/ProfileCard.jsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 import ProfileHeader from "./ProfileHeader/ProfileHeader";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Container = styled.article`
 	font-size: 1em;
+    background: ${({ theme }) => theme.colors.primary[2]};
+    width: max-content;
+    height: max-content;
     ${({ theme }) => theme.others.borderRadius}
+    ${({ theme }) => theme.effects.dropShadow}
 `;
 
 const ProfileCard = ({
@@ -16,7 +21,10 @@ const ProfileCard = ({
 				icons={icons}
 				profilePicOpts={profilePicOpts}
 			/>
-			<section></section>
+			<ProfileInfo
+				name="Wawan"
+				nim={15000120120008}
+			/>
 		</Container>
 	);
 };
---
+++ src/components/UI/ProfileCard/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+    padding: 0.8em 0.5em;
+    display: flex;
+    flex-direction: column;
+    row-gap: 1.2em;
+`;
+
+const Name = styled.p`
+    font: 900 1.6em "Lato", sans-serif;
+    line-height: unset;
+    height: 100%;
+`;
+
+const Nim = styled.small`
+    ${({ theme }) => theme.text.note};
+    line-height: unset;
+    text-align: right;
+`;
+
+const ProfileInfo = ({ name, nim }) => {
+	return (
+		<Container>
+			<Name>{name}</Name>
+			<Nim>{nim}</Nim>
+		</Container>
+	);
+};
+
+export default ProfileInfo;
