e2d1502b02183e5473d3fb75c8fb14d2eb09ad43
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-12 16:46:44 +0700

    Create `TranslateBtn`

---
+++ src/components/UI/TranslateBtn.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+	${({ theme }) => theme.effects.acrylicWShadow}
+	${({ theme }) => theme.text.titleCard}
+    ${({ theme }) => theme.others.borderRadius}
+
+    border: none;
+	outline: none;
+	margin: 0;
+	padding: 0;
+	color: ${({ theme }) => theme.colors.primary[2]};
+	cursor: pointer;
+
+	font-size: 1.3em;
+	line-height: unset;
+
+	width: 1.5385em;
+	height: 1.5385em;
+`;
+
+const Text = styled.p`
+	background: linear-gradient(90deg, #ffffff 0%, rgba(255, 255, 255, 0) 100%);
+	-webkit-background-clip: text;
+	background-clip: text;
+	color: transparent;
+`;
+
+const TranslateBtn = ({ onClick, children: text, className = "" }) => {
+	text = text.slice(0, 2).toUpperCase();
+
+	return (
+		<Button
+			className={`translate-btn ${className}`}
+			onClick={onClick}
+		>
+			<Text>{text}</Text>
+		</Button>
+	);
+};
+
+export default TranslateBtn;
