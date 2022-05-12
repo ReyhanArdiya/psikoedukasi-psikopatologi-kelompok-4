dd793b8eeef0601d052fef5ef85d2cd99f154535
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-12 15:36:57 +0700

    Create `ProfileLinks`

---
+++ src/components/UI/ProfileCard/ProfileHeader/ProfileLinks.jsx
@@ -0,0 +1,60 @@
+import { v4 as uuidv4 } from "uuid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const Container = styled.ul`
+    color: ${({ theme }) => theme.colors.secondary[3]};
+    display: flex;
+    gap: 1em;
+    height: max-content;
+    width: max-content;
+    list-style: none;
+`;
+
+const StyledLink = styled.a`
+    color: inherit;
+`;
+
+const StyledFa = styled(FontAwesomeIcon)`
+    font-size: 1.5em;
+    height: 1em;
+    min-height: 15px;
+    min-width: 15px;
+    width: 1em;
+`;
+
+/**
+ *
+ * @param {{
+ * icons:[{icon:SVGElement, href:string}],
+ * }} props
+ *
+ * @returns
+ */
+const ProfileLinks = ({ icons, className }) => {
+	const items = icons.map(({ href, icon }) => {
+		return (
+			<li
+				className="profile-link"
+				key={uuidv4()}
+			>
+				<StyledLink
+					href={href}
+					target={0}
+				>
+					<StyledFa icon={icon}/>
+				</StyledLink>
+			</li>
+		);
+	});
+
+	return (
+		<Container
+			className={`profile-links ${className}`}
+		>
+			{items}
+		</Container>
+	);
+};
+
+export default ProfileLinks;
