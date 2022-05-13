29e5fad45ae7a5c701a8bb69298283d5940db5a3
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-13 16:27:58 +0700

    Change `App` container w&h

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,9 +13,7 @@ const GlobalStyle = createGlobalStyle`
 const Container = styled.div`
 	background: ${({ theme }) => theme.colors.primary[1]};
 	min-height: 600px;
-	height: 100vh;
 	min-width: 320px;
-	width: 100vw;
 `;
 
 const App = () => {
