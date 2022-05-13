43b75e9322d9d08f19a55ca25d738c65f77153fb
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-13 15:21:09 +0700

    Remove `Transition` folder

--- src/components/UI/Animations/BlurToClear.jsx
+++ src/components/UI/Animations/BlurToClear.jsx
@@ -1,9 +1,9 @@
 import { CSSTransition } from "react-transition-group";
-import { baseTransitionMs } from "../../../../styles/csstransition-animations";
+import { baseTransitionMs } from "../../../styles/csstransition-animations";
 import { useInView } from "react-intersection-observer";
 import React from "react";
 
-const FadeInOut = ({ children, options, infinite = false }) => {
+const BlurToClear = ({ children, options, infinite = false }) => {
 	const { ref, inView } = useInView({
 		initialInView : true,
 		threshold     : 1,
@@ -14,16 +14,16 @@ const FadeInOut = ({ children, options, infinite = false }) => {
 	return (
 		<CSSTransition
 			appear={true}
-			classNames="fade-in-out"
+			classNames="blur-to-clear"
 			in={inView}
 			timeout={baseTransitionMs * 2}
 		>
 			{React.cloneElement(children, {
-				className : "fade-in-out",
+				className : "blur-to-clear",
 				ref,
 			})}
 		</CSSTransition>
 	);
 };
 
-export default FadeInOut;
+export default BlurToClear;
--- src/components/UI/Animations/FadeInOut.jsx
+++ src/components/UI/Animations/FadeInOut.jsx
@@ -1,9 +1,9 @@
 import { CSSTransition } from "react-transition-group";
-import { baseTransitionMs } from "../../../../styles/csstransition-animations";
+import { baseTransitionMs } from "../../../styles/csstransition-animations";
 import { useInView } from "react-intersection-observer";
 import React from "react";
 
-const BlurToClear = ({ children, options, infinite = false }) => {
+const FadeInOut = ({ children, options, infinite = false }) => {
 	const { ref, inView } = useInView({
 		initialInView : true,
 		threshold     : 1,
@@ -14,16 +14,16 @@ const BlurToClear = ({ children, options, infinite = false }) => {
 	return (
 		<CSSTransition
 			appear={true}
-			classNames="blur-to-clear"
+			classNames="fade-in-out"
 			in={inView}
 			timeout={baseTransitionMs * 2}
 		>
 			{React.cloneElement(children, {
-				className : "blur-to-clear",
+				className : "fade-in-out",
 				ref,
 			})}
 		</CSSTransition>
 	);
 };
 
-export default BlurToClear;
+export default FadeInOut;
