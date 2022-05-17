f311388ae3cbd6e7fa2af29011fb0dd8eb7f3850
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-17 20:51:54 +0700

    Animate `Intermezzo`s

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import HomeHero from "./HomeHero";
 import React from "react";
 import Intermezzo from "./Intermezzo";
 import { Text } from "../../components/UI/Content/Texts";
+import BlurToClear from "../../components/UI/Animations/BlurToClear";
 
 const Container = styled.div``;
 
@@ -14,61 +15,89 @@ const MultilineText = styled.div`
 	justify-content: center;
 `;
 
-const Home = ({ propsDestruct }) => {
+const Home = () => {
 	return (
 		<Container id="Home">
 			<HomeHero />
 			<Intermezzo>
-				<Text>
-					2 gangguan yang akan kami kenalkan pertama kali berhubungan
-					dengan perasaan depresi.
-				</Text>
+				<BlurToClear>
+					<Text>
+						2 gangguan yang akan kami kenalkan pertama kali
+						berhubungan dengan perasaan depresi.
+					</Text>
+				</BlurToClear>
 			</Intermezzo>
 			<Intermezzo>
-				<Text>
-					JIka sebelumnya kita telah berjumpa dengan perasaan depresi,
-					bagaimana dengan gangguan yang melibatkannya dengan perasaan
-					manik?
-				</Text>
+				<BlurToClear>
+					<Text>
+						Jika sebelumnya kita telah berjumpa dengan perasaan
+						depresi, bagaimana dengan gangguan yang melibatkannya
+						dengan perasaan manik?
+					</Text>
+				</BlurToClear>
 			</Intermezzo>
 			<Intermezzo>
-				<Text>
-					Apakah hanya BD saja yang melibatkan kedua perasaan itu?
-				</Text>
+				<BlurToClear>
+					<Text>
+						Apakah hanya BD saja yang melibatkan kedua perasaan itu?
+					</Text>
+				</BlurToClear>
 			</Intermezzo>
 			<Intermezzo>
-				<Text>Tidak.</Text>
+				<BlurToClear>
+					<Text>Tidak.</Text>
+				</BlurToClear>
 			</Intermezzo>
 			<Intermezzo>
-				<Text>Sebelum kita selesai</Text>
+				<BlurToClear>
+					<Text>Sebelum kita selesai</Text>
+				</BlurToClear>
 			</Intermezzo>
 			<Intermezzo>
-				<Text>Dan kita selesai</Text>
+				<BlurToClear>
+					<Text>Dan kita selesai</Text>
+				</BlurToClear>
 			</Intermezzo>
 			<Intermezzo>
 				<MultilineText>
-					<Text>Sebelum kita menutupi</Text>
-					<Text>Ada sedikit pesan</Text>
+					<BlurToClear>
+						<Text>Sebelum kita menutupi</Text>
+					</BlurToClear>
+					<BlurToClear>
+						<Text>Ada sedikit pesan</Text>
+					</BlurToClear>
 				</MultilineText>
 			</Intermezzo>
 			<Intermezzo>
 				<MultilineText>
-					<Text>Walaupun Anda sudah mengenal beberapa gangguan</Text>
-					<Text>
-						Anda masih bisa mempelajarinya lebih lanjut dari
-						sumber-sumber terpecaya lainnya
-					</Text>
-					<Text>
-						Walaupun Anda sudah memahami kriteria diagnostik menurut
-						DSM-V dari laman ini
-					</Text>
-					<Text>
-						Jangan mencoba untuk melakukan self-diagnose, ya!
-					</Text>
+					<BlurToClear>
+						<Text>
+							Walaupun Anda sudah mengenal beberapa gangguan
+						</Text>
+					</BlurToClear>
+					<BlurToClear>
+						<Text>
+							Anda masih bisa mempelajarinya lebih lanjut dari
+							sumber-sumber terpecaya lainnya
+						</Text>
+					</BlurToClear>
+					<BlurToClear>
+						<Text>
+							Walaupun Anda sudah memahami kriteria diagnostik
+							menurut DSM-V dari laman ini
+						</Text>
+					</BlurToClear>
+					<BlurToClear>
+						<Text>
+							Jangan mencoba untuk melakukan self-diagnose, ya!
+						</Text>
+					</BlurToClear>
 				</MultilineText>
 			</Intermezzo>
 			<Intermezzo>
-				<Text>bye.</Text>
+				<BlurToClear>
+					<Text>bye.</Text>
+				</BlurToClear>
 			</Intermezzo>
 		</Container>
 	);
