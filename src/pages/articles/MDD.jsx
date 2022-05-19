a9ca3518d6a0ca330db26c066817b15c478c877b
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-19 09:44:00 +0700

    Animate `MDD`

--- src/pages/articles/MDD.jsx
+++ src/pages/articles/MDD.jsx
@@ -1,4 +1,3 @@
-// import styled from "styled-components";
 import React from "react";
 import ArticlePage from "../../components/UI/Layouts/ArticlePage/ArticlePage";
 import mountainSM from "../../images/content/small/mountain-640w.jpg";
@@ -11,6 +10,7 @@ import Ol from "../../components/UI/Layouts/List/Ol";
 import Li from "../../components/UI/Layouts/List/Li";
 import Note from "../../components/UI/Content/Note/Note";
 import SwitchContent from "../../components/UI/Animations/SwitchContent";
+import BlurToClear from "../../components/UI/Animations/BlurToClear";
 
 const MDD = () => {
 	const { t, i18n } = useTranslation("MDD");
@@ -25,87 +25,65 @@ const MDD = () => {
 			title="Major Depressive Disorder (MDD)"
 		>
 			<ArticleSection>
-				<H2>Depresi &amp; Perasaan Sedih</H2>
-				<Text>
-					Faktanya, tidak semua kondisi sedih dapat dikategorikan
-					sebagai bagian dari depresi. Pada dasarnya seseorang mungkin
-					pernah mengalami kondisi kegagalan yang berujung pada
-					kesedihan mendalam. Namun, perlu diketahui bahwa kesedihan
-					berbeda dengan gangguan depresi. Tidak semua perasaan sedih
-					akan menimbulkan depresi. Oleh karenanya, perlu ada
-					pertimbangan cermat terkait gejala depresi sesuai DSM dan
-					intervensi klinis untuk mengetahui jenis gangguan yang
-					dialami seseorang.
-				</Text>
-				<Text>
-					Menurut Oltmans & Emery (dalam Chandrawijaya & Yunanto,
-					2019) Major Depressive Disorder (MDD) merupakan salah satu
-					bentuk gangguan mood (suasana hati) dimana kondisi individu
-					didominasi oleh perasaan tertekan (depresi).
-				</Text>
+				<BlurToClear>
+					<H2>Depresi &amp; Perasaan Sedih</H2>
+				</BlurToClear>
+				<BlurToClear>
+					<Text>
+						Faktanya, tidak semua kondisi sedih dapat dikategorikan
+						sebagai bagian dari depresi. Pada dasarnya seseorang
+						mungkin pernah mengalami kondisi kegagalan yang berujung
+						pada kesedihan mendalam. Namun, perlu diketahui bahwa
+						kesedihan berbeda dengan gangguan depresi. Tidak semua
+						perasaan sedih akan menimbulkan depresi. Oleh karenanya,
+						perlu ada pertimbangan cermat terkait gejala depresi
+						sesuai DSM dan intervensi klinis untuk mengetahui jenis
+						gangguan yang dialami seseorang.
+					</Text>
+				</BlurToClear>
+				<BlurToClear>
+					<Text>
+						Menurut Oltmans & Emery (dalam Chandrawijaya & Yunanto,
+						2019) Major Depressive Disorder (MDD) merupakan salah
+						satu bentuk gangguan mood (suasana hati) dimana kondisi
+						individu didominasi oleh perasaan tertekan (depresi).
+					</Text>
+				</BlurToClear>
 			</ArticleSection>
 
-			<ArticleSection>
-				<H2>Kriteria DSM-V MDD</H2>
-				<TranslateButtons
-					i18n={i18n}
-					t={t}
-				/>
-				<SwitchContent transitionKey={i18n.resolvedLanguage}>
-					<Ol>
-						<Li>
-							{t("A.text")}
-							<Ol>
-								<Li>
-									{t("A.nest.1")}
-								</Li>
-								<Li>
-									{t("A.nest.2")}
-								</Li>
-								<Li>
-									{t("A.nest.3")}
-								</Li>
-								<Li>
-									{t("A.nest.4")}
-								</Li>
-								<Li>
-									{t("A.nest.5")}
-								</Li>
-								<Li>
-									{t("A.nest.6")}
-								</Li>
-								<Li>
-									{t("A.nest.7")}
-								</Li>
-								<Li>
-									{t("A.nest.8")}
-								</Li>
-								<Li>
-									{t("A.nest.9")}
-								</Li>
-							</Ol>
-						</Li>
-						<Li>
-							{t("B.text")}
-						</Li>
-						<Li>
-							{t("C.text")}
-						</Li>
-						<Note>
-							{t("Note1.text")}
-						</Note>
-						<Note>
-							{t("Note2.text")}
-						</Note>
-						<Li>
-							{t("D.text")}
-						</Li>
-						<Li>
-							{t("E.text")}
-						</Li>
-					</Ol>
-				</SwitchContent>
-			</ArticleSection>
+			<BlurToClear>
+				<ArticleSection>
+					<H2>Kriteria DSM-V MDD</H2>
+
+					<TranslateButtons
+						i18n={i18n}
+					/>
+					<SwitchContent transitionKey={i18n.resolvedLanguage}>
+						<Ol>
+							<Li>
+								{t("A.text")}
+								<Ol>
+									<Li>{t("A.nest.1")}</Li>
+									<Li>{t("A.nest.2")}</Li>
+									<Li>{t("A.nest.3")}</Li>
+									<Li>{t("A.nest.4")}</Li>
+									<Li>{t("A.nest.5")}</Li>
+									<Li>{t("A.nest.6")}</Li>
+									<Li>{t("A.nest.7")}</Li>
+									<Li>{t("A.nest.8")}</Li>
+									<Li>{t("A.nest.9")}</Li>
+								</Ol>
+							</Li>
+							<Li>{t("B.text")}</Li>
+							<Li>{t("C.text")}</Li>
+							<Note>{t("Note1.text")}</Note>
+							<Note>{t("Note2.text")}</Note>
+							<Li>{t("D.text")}</Li>
+							<Li>{t("E.text")}</Li>
+						</Ol>
+					</SwitchContent>
+				</ArticleSection>
+			</BlurToClear>
 		</ArticlePage>
 	);
 };
