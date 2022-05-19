757d198460fd22ca1d10bc95ad03167b8a69da25
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-19 16:26:58 +0700

    Add `Manic` DSM-V

--- src/pages/articles/BD/BD.jsx
+++ src/pages/articles/BD/BD.jsx
@@ -7,14 +7,17 @@ import BlurToClear from "../../../components/UI/Animations/BlurToClear";
 import { H2, /* Text */ } from "../../../components/UI/Content/Texts";
 import TranslateButtons from "../../../components/UI/Buttons/TranslateButtons";
 import SwitchContent from "../../../components/UI/Animations/SwitchContent";
-import Ol from "../../../components/UI/Layouts/List/Ol";
-import Li from "../../../components/UI/Layouts/List/Li";
-import Note from "../../../components/UI/Content/Note/Note";
 import BDContent from "./BDContent";
 import ArticleSection from "../../../components/UI/Layouts/ArticlePage/ArticleSection";
+import Manic from "./DSM/Manic";
+import styled from "styled-components";
+
+const BDI = styled(ArticleSection)`
+    width: 100%;
+`;
 
 const BD = () => {
-	const { t, i18n } = useTranslation("BD");
+	const { i18n } = useTranslation("BD");
 
 	return (
 		<ArticlePage
@@ -33,37 +36,9 @@ const BD = () => {
 					<TranslateButtons i18n={i18n} />
 
 					<SwitchContent transitionKey={i18n.resolvedLanguage}>
-						<Ol>
-							<Li>{t("A.text")}</Li>
-							<Li>
-								{t("B.text")}
-								<Ol>
-									<Li>{t("B.nest.1")}</Li>
-									<Li>{t("B.nest.2")}</Li>
-									<Li>{t("B.nest.3")}</Li>
-									<Li>{t("B.nest.4")}</Li>
-								</Ol>
-							</Li>
-							<Li>
-								{t("C.text")}
-								<Ol>
-									<Li>{t("C.nest.1")}</Li>
-									<Li>{t("C.nest.2")}</Li>
-									<Li>{t("C.nest.3")}</Li>
-									<Li>{t("C.nest.4")}</Li>
-									<Li>{t("C.nest.5")}</Li>
-									<Li>{t("C.nest.6")}</Li>
-									<Li>{t("C.nest.7")}</Li>
-								</Ol>
-							</Li>
-							<Note>{t("Note1.text")}</Note>
-							<Li>{t("D.text")}</Li>
-							<Li>{t("E.text")}</Li>
-							<Li
-								dangerouslySetInnerHTML={{ __html : t("F.text") }}
-							/>
-							<Li>{t("G.text")}</Li>
-						</Ol>
+						<BDI id="BD-I">
+							<Manic />
+						</BDI>
 					</SwitchContent>
 				</ArticleSection>
 			</BlurToClear>
---
+++ src/pages/articles/BD/DSM/Manic.jsx
@@ -0,0 +1,43 @@
+import { H2 } from "../../../../components/UI/Content/Texts";
+import ArticleSection from "../../../../components/UI/Layouts/ArticlePage/ArticleSection";
+import Ol from "../../../../components/UI/Layouts/List/Ol";
+import styled from "styled-components";
+import Li from "../../../../components/UI/Layouts/List/Li";
+import { useTranslation } from "react-i18next";
+import Note from "../../../../components/UI/Content/Note/Note";
+
+const Container = styled(ArticleSection)`
+    width: 100%;
+`;
+
+const Manic = () => {
+	const { t } = useTranslation("Manic");
+
+	return (
+		<Container>
+			<H2>Episode Manik</H2>
+
+			<Ol>
+				<Li>{t("A.text")}</Li>
+				<Li>
+					{t("B.text")}
+					<Ol>
+						<Li>{t("B.nest.1")}</Li>
+						<Li>{t("B.nest.2")}</Li>
+						<Li>{t("B.nest.3")}</Li>
+						<Li>{t("B.nest.4")}</Li>
+						<Li>{t("B.nest.5")}</Li>
+						<Li>{t("B.nest.6")}</Li>
+						<Li>{t("B.nest.7")}</Li>
+					</Ol>
+				</Li>
+				<Li>{t("C.text")}</Li>
+				<Li>{t("D.text")}</Li>
+				<Note>{t("Note1.text")}</Note>
+				<Note>{t("Note2.text")}</Note>
+			</Ol>
+		</Container>
+	);
+};
+
+export default Manic;
