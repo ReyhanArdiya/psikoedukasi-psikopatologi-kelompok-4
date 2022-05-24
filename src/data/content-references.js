5767cfc10d909c75da3b8fc6f3100ac16d9ef2cc
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-24 10:30:27 +0700

    Create `content-references`

---
+++ src/data/content-references.js
@@ -0,0 +1,5 @@
+const contentReferences = [
+	"Chandrawijaya, E, F., & Yunanto, T, A, R. (2021). Studi kasus: dinamika psikologis anak angkat yang mengalami gangguan depresi mayor tingkat berat dengan fitur melankolis dan psikotik. Jurnal Insight Fakultas Psikologi Universitas Muhammadiyah, 17(2), 327-337"
+];
+
+export default contentReferences;
