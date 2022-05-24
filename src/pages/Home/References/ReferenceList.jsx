e9f0f3ff7d27fdfcb0ddbd68b07de61b8e32158f
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-24 10:52:25 +0700

    Sort `references`

--- src/pages/Home/References/ReferenceList.jsx
+++ src/pages/Home/References/ReferenceList.jsx
@@ -1,16 +1,20 @@
 import styled from "styled-components";
 import Li from "../../../components/UI/Layouts/List/Li";
 import Ol from "../../../components/UI/Layouts/List/Ol";
+import { v4 as uuidv4 } from "uuid";
 
 const Container = styled(Ol)`
-    padding: 0;
+	padding: 0;
+    display: flex;
+    flex-direction: column;
+    row-gap: inherit;
 `;
 
 const ReferenceItem = styled(Li)`
-    list-style-type: none !important;
-    --indentation: 3em;
-    padding-left: var(--indentation);
-    text-indent: calc(-1 * var(--indentation));
+	list-style-type: none !important;
+	--indentation: 3em;
+	padding-left: var(--indentation);
+	text-indent: calc(-1 * var(--indentation));
 `;
 
 /**
@@ -20,9 +24,13 @@ const ReferenceItem = styled(Li)`
  * @returns
  */
 const ReferenceList = ({ references }) => {
-	const referenceItems = references.map(
-		reference => <ReferenceItem>{reference}</ReferenceItem>
-	);
+	const referenceItems = references
+		.sort((a, b) => a > b ? 1 : -1)
+		.map(
+			reference => <ReferenceItem key={uuidv4()}>
+				{reference}
+			</ReferenceItem>
+		);
 
 	return <Container>{referenceItems}</Container>;
 };
