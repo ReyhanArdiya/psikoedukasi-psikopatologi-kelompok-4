efdaffd37e773a79ad0b04b186c662058e076bfa
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-24 10:30:56 +0700

    Create `ReferenceList`

---
+++ src/pages/Home/References/ReferenceList.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import Li from "../../../components/UI/Layouts/List/Li";
+import Ol from "../../../components/UI/Layouts/List/Ol";
+
+const Container = styled(Ol)`
+    padding: 0;
+`;
+
+const ReferenceItem = styled(Li)`
+    list-style-type: none !important;
+    padding-left: 3em;
+    text-indent: -3em;
+
+`;
+
+/**
+ *
+ * @param {{references: string[]}} props
+ *
+ * @returns
+ */
+const ReferenceList = ({ references }) => {
+	const referenceItems = references.map(
+		reference => <ReferenceItem>{reference}</ReferenceItem>
+	);
+
+	return <Container>{referenceItems}</Container>;
+};
+
+export default ReferenceList;
