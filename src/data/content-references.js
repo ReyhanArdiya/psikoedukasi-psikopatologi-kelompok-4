e1ee0ecebbbfb36209acf7c22727540f9ab1b110
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-05-24 18:26:01 +0700

    Add `contentReferences` entry

--- src/data/content-references.js
+++ src/data/content-references.js
@@ -2,6 +2,7 @@ const contentReferences = [
 	"American Psychiatric Association. (2013). <i>Diagnostic and statistical manual of mental disorders (DSM-V)</i> (5th ed.). American Psychiatric Publishing.",
 	"Chandrawijaya, E, F., & Yunanto, T, A, R. (2021). Studi kasus: Dinamika psikologis anak angkat yang mengalami gangguan depresi mayor tingkat berat dengan fitur melankolis dan psikotik. <i>Jurnal Insight Fakultas Psikologi Universitas Muhammadiyah</i>, <i>17</i>(2), 327-337",
 	"Nevid, J. S., Rathus, S. A., & Greene, B. (2018). <i>Abnormal psychology in a changing world</i> (10th ed.). Pearson Higher Education.",
+	"Nasution, H. N., & Firdausi, H. (2021). Pendekatan diagnosis dan tatalaksana gangguan mood pada usia lanjut. <i>Jurnal Kedokteran</i>, <i>6</i>(2), 131-142."
 ];
 
 export default contentReferences;
